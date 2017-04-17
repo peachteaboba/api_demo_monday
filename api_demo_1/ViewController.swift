b3e8e269f394cce2a60ebe15908741dbc9ff286d
Andy Feng <anndyfeng1@gmail.com>
2017-04-17 10:38:09 -0700

    first commit

--- api_demo_1/ViewController.swift
+++ api_demo_1/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        makeAPICall()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +23,72 @@ class ViewController: UIViewController {
     }
 
 
+    func makeAPICall() {
+        
+        // specify the url that we will be sending the GET Request to
+        let url = URL(string: "http://swapi.co/api/people/")
+        
+        
+        
+        // create a URLSession to handle the request tasks
+        let session = URLSession.shared
+        
+        
+        
+        // create a "data task" to make the request and run the completion handler
+        let task = session.dataTask(with: url!, completionHandler: {
+            // see: Swift closure expression syntax
+            data, response, error in
+            
+            do {
+                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+//                    print(jsonResult)
+                    
+                    if let peepsArr = jsonResult["results"]{
+//                        print(peepsArr)
+                        
+                        if let pArr = peepsArr as? NSArray{
+                            
+                            for p in pArr {
+                                
+                                if let pDict = p as? NSDictionary {
+                                    
+                                    print(pDict["name"]!)
+                                }
+                                
+                                
+                            }
+                            
+                        }
+                        
+                      
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                }
+            } catch {
+                print(error)
+            }
+            
+        })
+        
+        
+        
+        
+        // execute the task and wait for the response before
+        // running the completion handler. This is async!
+        task.resume()
+        
+        
+    }
+    
+    
+    
+    
 }
 
